d5d51095f2860d3981a0980044fef3b57ef0f703
Eaocs <Owen.Stuttard@dal.ca>
2023-04-01 12:18:42 -0300

    Changed to standard main instead of arduino syntax

--- SenseTest1/src/main.cpp
+++ SenseTest1/src/main.cpp
@@ -11,32 +11,22 @@
 #include "Nicla_System.h"       
 
 
-void setup() {
+int main(void) {
   //run this code once when Nicla Sense ME board turns on
   nicla::begin();               // initialise library
   nicla::leds.begin();          // Start I2C connection
+
+
+  while(1) {
+    //run this code in a loop
+    nicla::leds.setColor(green);  //turn green LED on
+    delay(1000);                  //wait 1 second
+    nicla::leds.setColor(off);    //turn all LEDs off
+    delay(1000);                  //wait 1 second
+  }
+ return 0;  
 }
 
-void loop() {
-  //run this code in a loop
-  nicla::leds.setColor(green);  //turn green LED on
-  delay(1000);                  //wait 1 second
-  nicla::leds.setColor(off);    //turn all LEDs off
-  delay(1000);                  //wait 1 second
-}
-   
-
-// int main(void) {
-//   //run this code once when Nicla Sense ME board turns on
-//  pinMode(LEDR, OUTPUT);
-//   while(1){
-//     digitalWrite(LEDR, HIGH);
-//     osDelay(100);
-//   }
-
-
-// return 0;
-// }
 
 
 
