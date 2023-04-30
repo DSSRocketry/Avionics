2d412a8daa986dd8ca7fdb258dfbc30ee5732992
Eaocs <Owen.Stuttard@dal.ca>
2023-04-30 15:33:49 -0300

    Working Blink

--- SenseTest1/src/main.cpp
+++ SenseTest1/src/main.cpp
@@ -1,22 +1,13 @@
-#include <Arduino.h>
 
+
+#include <Arduino.h>
 // Intialise library which communicates with RGB driver
 // Functions accessible under 'nicla' namespace
-
-     
-
-// nicla board;
-
-
 #include "Nicla_System.h"       
-
-
 int main(void) {
   //run this code once when Nicla Sense ME board turns on
   nicla::begin();               // initialise library
   nicla::leds.begin();          // Start I2C connection
-
-
   while(1) {
     //run this code in a loop
     nicla::leds.setColor(green);  //turn green LED on
