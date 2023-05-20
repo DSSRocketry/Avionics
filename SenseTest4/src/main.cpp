9005b3492bf20fdbcd79af4acded9ca97c20c627
Ethan Johnston <77747068+ethanajohnston@users.noreply.github.com>
2023-05-20 12:16:11 -0300

    Cleanup

--- SenseTest4/src/main.cpp
+++ SenseTest4/src/main.cpp
@@ -1,17 +1,15 @@
-/* Example for blinking and sensor reading in two RTOS threads*/
-//Include the relevant libraries
-//Note that the BHY2 library requires the arduino BLE library to be
-//available in the project as well
+// Example for blinking and sensor reading in two RTOS threads
+// Include the relevant libraries
+// Note that the BHY2 library requires the arduino BLE library to be available in the project as well
+
 #include "Arduino.h"
 #include "Nicla_System.h"
 #include "Arduino_BHY2.h"
 
-//This namespace allows use of time in simple formats
-// such as 1s for 1 second
+// This namespace allows use of time in simple formats
 using namespace std::chrono_literals;
 
-
-//declare the sesors
+// Declare the sensor objects
 SensorXYZ accel(SENSOR_ID_ACC);
 SensorXYZ gyro(SENSOR_ID_GYRO);
 Sensor temp(SENSOR_ID_TEMP);
@@ -19,44 +17,24 @@ Sensor gas(SENSOR_ID_GAS);
 Sensor pressure(SENSOR_ID_BARO);
 SensorQuaternion rotation(SENSOR_ID_RV);
 
-//declare the threads
+// Declare the threads with priorities
+rtos::Thread blinkthread(osPriorityNormal);//heartbeat blink is wanted
+rtos::Thread sensorprintthread(osPriorityRealtime);//sensor updating will happily consume all time it seems
 
-rtos::Thread blinkthread(osPriorityAboveNormal);//heartbeat blink is wanted
-rtos::Thread sensorupdatethread;//not actually used currently
-rtos::Thread sensorprintthread;//sensor updating will happily consume all time it seems
-//some testing remains with the thread priority. below normal seems to cause threads to not run
-
-/**function to run in a thread and blink the onboard led
- */
-void blink_app(void)
-{
+// Blink led
+void blink(void) {
   while(1) {
-    nicla::leds.setColor(green);                       //turn green LED on
-    rtos::ThisThread::sleep_for(1s);                   //wait 1 second
-    nicla::leds.setColor(off);                         //turn all LEDs off
-    rtos::ThisThread::sleep_for(1s);                   //wait 1 second
+    nicla::leds.setColor(red);                       //turn green LED on
+    rtos::ThisThread::sleep_for(1s);                 //wait 1 second
+    nicla::leds.setColor(off);                       //turn all LEDs off
+    rtos::ThisThread::sleep_for(1s);                 //wait 1 second
   }
 }
 
-/**function to run in a thread and update sensor data.
- * You want to update it constantly
- */
-void sensorupdate_app(void){
-  BHY2.update();
-
-}
-
-
-
-
-/**function to run in a thread and print sensor data to the serial port
- * you only want to print every second
- */
-void sensorprint_app(void)
-{
-
+// Update sensor data and print to serial monitor
+void sensorprint(void) {
   while(1) {
-    rtos::ThisThread::sleep_for(1s);                   //wait 1 second
+    rtos::ThisThread::sleep_for(500ms);                   //wait 500ms (Using chrono duration)
     BHY2.update();
     Serial.println(String("acceleration: ") + accel.toString());
     Serial.println(String("gyroscope: ") + gyro.toString());
@@ -64,20 +42,23 @@ void sensorprint_app(void)
     Serial.println(String("gas: ") + String(gas.value(),3));
     Serial.println(String("pressure: ") + String(pressure.value(),3));
     Serial.println(String("rotation: ") + rotation.toString());
+    
+    // 
     Serial.println(millis());
   }
 }
 
-void setup(void)
-{
-  //run this code once when Nicla Sense ME board turns on
+// setup() runs once on the first power up of the board.
+void setup(void) {
+
   nicla::begin();               // initialise library
   nicla::leds.begin();          // Start I2C connection
   Serial.begin(115200);
   while(!Serial);
   Serial.println(String("log msg 1")); //example of sending a message to debug with serial
-  BHY2.begin();
 
+  // Intialize 
+  BHY2.begin();
   accel.begin();
   gyro.begin();
   temp.begin();
@@ -85,14 +66,11 @@ void setup(void)
   rotation.begin();
   pressure.begin();
 
-  blinkthread.start(blink_app);
-  //sensorupdatethread.start(sensorupdate_app);
-  sensorprintthread.start(sensorprint_app);
-  blinkthread.join(); //wait for blink to end (aka never return to main)
+  // Make threads run associated functions
+  blinkthread.start(blink);
+  sensorprintthread.start(sensorprint);
 }
 
-
-void loop(void){
-  
+void loop(void) {
+  // DO NOT PUT ANYTHING HERE!
 }
-
